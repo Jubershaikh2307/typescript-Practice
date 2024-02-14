c0099ca3ec630dc289fb636a90e7d4825a865b14
Juber Shaikh <jubershaikh2307@gmail.com>
2024-02-14 23:42:29 +0530

    reverse the string in the typescript

--- Day-1/index.js
+++ Day-1/index.js
@@ -1,10 +1,22 @@
 // function display(a: number) {
 //     console.log(a)
 // }
-var data = [
-    {
-        name: "XUV",
-        wheel: 4
-    }
-];
-console.log(data);
+// display(1)
+// type Car = {
+//     name: string,
+//     wheel:Number
+// }
+// let data: Car[] = [
+//     {
+//         name: "XUV",
+//         wheel:4
+//     }
+// ]
+// console.log(data)
+// reverse the string
+var str = " juber";
+var temp = "";
+for (var i = str.length - 1; i >= 0; i--) {
+    temp += str[i];
+}
+console.log(temp);
--- Day-1/index.ts
+++ Day-1/index.ts
@@ -4,16 +4,28 @@
 
 // display(1)
 
-type Car = {
-    name: string,
-    wheel:Number
+// type Car = {
+//     name: string,
+//     wheel:Number
+// }
+
+// let data: Car[] = [
+//     {
+//         name: "XUV",
+//         wheel:4
+//     }
+// ]
+
+// console.log(data)
+
+// reverse the string
+
+let str: string = " juber"
+
+let temp: string = ""
+
+for (let i = str.length-1; i >=0; i--){
+    temp+=str[i]
 }
 
-let data: Car[] = [
-    {
-        name: "XUV",
-        wheel:4
-    }
-]
-
-console.log(data)
+console.log(temp)
