895231379943a733faf503cf6728bb05b3803d30
Juber Shaikh <jubershaikh2307@gmail.com>
2024-02-16 17:40:35 +0530

    annotation

---
+++ Day-2/Annotations/index.ts
@@ -0,0 +1,49 @@
+let apples: number = 5 
+let name_f: string = "Juber"
+
+let hasName: boolean = true
+
+let nothingMuch: null = null
+let nothing: undefined = undefined
+
+// Build In Operators
+
+let now: Date = new Date()
+
+// Array
+
+let colors: string[] = ["red"]
+
+let arr: number[] = [1, 2, 3]
+
+let arrBoolean: boolean[] = [true, false]
+
+// Classes
+
+class Car{
+    
+}
+
+let car: Car = new Car()
+
+// Object Literal
+
+let points: { x: number; y:number} = {
+    x: 10,
+    y:10
+}
+
+// function
+
+const logNumber: (i: number) => void = (i: number) => {
+    console.log(i)
+}
+
+logNumber(1)
+
+
+
+
+
+
+
